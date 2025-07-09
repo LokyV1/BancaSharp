cd1573bbcfda33e34a7b118649ff6d7968605f2f
LokyV1 <luigilok24@gmail.com>
2025-07-09 11:06:13 +0200

    Add project files.

---
+++ BancaSharp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace BancaSharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to BancaSharp!");
+        }
+    }
+}
